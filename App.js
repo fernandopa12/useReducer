c9d2ca4619bb9dab9b58bed225a25da76ac4ae43
Logon PF Fiap <logonpflocal@fiap.com.br>
2024-04-11 11:44:50 -0300

    Uso do useReducer

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useReducer } from 'react';
+
+
+const reducer = (state,action)=>{
+  switch(action.tipo){
+    case 'setText':
+      return {...state,texto:action.payload}
+    case 'setSobrenome':
+      return {...state,sobrenome:action.payload}
+    default:
+      return state;
+  }
+}
+
+const valorInicial = {
+  texto:'',
+  sobrenome:''
+}
 
 export default function App() {
+  let textoDigitado;
+  let textoSobrenome;
+
+  const[state,dispacth]=useReducer(reducer,valorInicial);
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>useReduce</Text>
+      <Text>{state.texto}</Text>
+      <Text>{state.sobrenome}</Text>
+      
+      <TextInput 
+        style={styles.input}
+        placeholder='Digite seu nome'
+        onChangeText={(texto)=>textoDigitado=texto}
+      />
+
+      <TextInput 
+        style={styles.input}
+        placeholder='Digite seu sobrenome'
+        onChangeText={(texto)=>textoSobrenome=texto}
+      />
+
+      <Button 
+        title='Alterar Nome'
+        onPress={()=>dispacth({tipo:'setText',payload:textoDigitado})}       
+      />
+
+      <Button 
+        title='Alterar Sobrenome'
+        onPress={()=>dispacth({tipo:'setSobrenome',payload:textoSobrenome})}       
+      />
+    
     </View>
   );
 }
@@ -16,5 +64,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap:15
   },
+  input:{
+    width:300,
+    height:50,
+    borderWidth:1,
+    borderRadius:10
+  }
 });
